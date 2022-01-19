98038c719d6b66f8ee93f1351a7a00f5753c7053
Jayapriya Pai <slashpai9@gmail.com>
2022-01-19 20:16:47 +0530

    Update README and cloud-init script

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ VM setup using multipass for working with ebpf in go
 
 [multpass](https://multipass.run) should be installed
 
+## Usage
+
+Clone this repo and run following commands
+
 ```bash
 multipass launch -n ebpf-vm --cloud-init cloud-config.yaml
 ```
@@ -14,4 +18,11 @@ To get shell to vm
 
 ```bash
 multipass shell ebpf-vm
+```
+
+To delete vm and cleanup
+
+```bash
+multipass delete ebpf-vm
+multipass purge
 ```
--- cloud-init.sh
+++ cloud-init.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 echo "apt-update"
 sudo apt-get update
 
-echo "install ebpf tools and dependencies"
-sudo apt-get install make clang llvm libbpf-dev libelf-dev
-
 echo "# install go"
 VERSION='1.17'
 OS='linux'
@@ -17,5 +14,12 @@ sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'PATH="$PATH:/usr/local/go/bin:'$HOME'/go/bin"' >> ~/.bash_aliases
+
+echo "install ebpf tools and dependencies"
+sudo apt install make
+sudo apt install clang
+sudo apt install llvm
+sudo apt install libbpf-dev
+sudo apt install libelf-dev
 
 echo "# package installations complete!"
